87dd00ccdf694a75e216a7744118abbdc1072113
Michelle Horn <michellehorn@live.com>
2022-03-29 18:03:14 -0300

    aumenta area alertas

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,14 +2,14 @@ import styled from "styled-components";
 import { colors } from "../../theme";
 
 const AlertSection = styled.section`
-  height: 130px;
+  height: 250px;
   width: 100%;
   background: ${colors.gray};
   margin-top: 1em;
   overflow-x: auto;
   @media (max-width: 500px) {
     margin-top: .5em;
-    height: 80px;
+    height: 200px;
   }
 `;
 
